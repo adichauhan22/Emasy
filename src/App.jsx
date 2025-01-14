9ef4aacc70a99693aee5578830838d3af17d300b
Aditya Chauhan <adityachauhan71804@gmail.com>
2025-01-14 15:01:50 +0530

    Edited frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { useState } from 'react'
-import Layout from './components/Layout/Layout'
+import Layout from './pages/Layout'
 import Login from './pages/Login'
 import Signup from './pages/Signup'  // Add this import
 import Dashboard from './pages/Dashboard'
